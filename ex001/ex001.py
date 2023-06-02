23a182a57acf9124be214c36f9e028f5dc0267d6
GiovanniAM <125399281+giocodess@users.noreply.github.com>
2023-06-02 12:02:47 -0300

    atualização

--- ex001/ex001.py
+++ ex001/ex001.py
@@ -1,11 +1,11 @@
-a = 14
-for i in range(30):
-    if a % 2 == 0:
-        a+= 1
+a = 14 
+for i in range(30): 
+    if a % 2 == 0: 
+        a+= 1 
         continue
-    else:   
-        if a % 5 == 0:
-            break
-        else:
-            a += 3
+    else: 
+        if a % 5 == 0: 
+            break 
+        else: a += 3 
+
 print(a)
